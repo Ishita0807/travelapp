320b180f031d7661ed2628065712f0361d0aad3d
Ishita Majumdar <majumdar.ishita2003@gmail.com>
2024-02-08 00:03:15 +0530

    Refactor Navbar component and add Button component

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,23 +1,34 @@
 import { NAV_LINKS } from '@/constants'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import Button from './Button'
 
- const Navbar = () => {
-  console.log({NAV_LINKS})  
+const Navbar = () => {
+  console.log({ NAV_LINKS })  
   return (
-    <nav className="flexBetween max-container padding-container relative z-30 py-5 ">
+    <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
         <Image src="/hilink-logo.svg" alt="logo" width={74} height={29}/>
-        <ul className='hidden gap-12 h-full lg:flex'>
-          {NAV_LINKS.map((link, index) => (
-              <Link href={link.href} key={link.key} className='flex regular-16 flexCenter cursor-pointer pb-1.5 text-gray-500'>
-              {link.label}
-              </Link>
-          ))}
-        </ul>
       </Link>
+      <ul className='hidden gap-12 h-full lg:flex'>
+        {NAV_LINKS.map((link, index) => (
+          <li key={link.key}>
+            <Link href={link.href} className='flex regular-16 flexCenter cursor-pointer pb-1.5 text-gray-500 hover:font-bold'>
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <div className='lg:flexCenter hidden'>
+        <Button
+          type="button"
+          title="Login"
+          icon="/user.svg"
+          variant="btn_dark_green"
+        />  
+      </div>     
     </nav>
   )
 }
+
 export default Navbar;
