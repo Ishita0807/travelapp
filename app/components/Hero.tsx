3ae2686678ecdf6bc7a7bb8c5b50a8b6d99b6dfd
Ishita Majumdar <majumdar.ishita2003@gmail.com>
2024-02-16 00:22:30 +0530

    Add star images and update review count

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -19,7 +19,25 @@ const Hero = () => {
        </p>
        <div className="my-11 flex flex-wrap gap-5">
         <div className="flex items-center gap-2">
-       </div>
+          {Array(5).fill(1).map((_, index) => (
+              <Image 
+                src="/star.svg"
+                key={index}
+                alt="star"
+                width={24}
+                height={24}
+               />
+          ))} 
+
+        </div>
+        <p className="bold-16 lg:bold-20 text-blue-70">
+            198k
+            <span className="regular-16 lg:regular-20 ml-1">Excellent Reviews</span>
+        </p>
+
+
+
+
 
         </div>
 
